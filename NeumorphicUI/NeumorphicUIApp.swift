62c6d9fb135cdb1f8ff49f83f84568eb80853c8b
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-09 20:24:53 +0530

    Updated

---
+++ NeumorphicUI/NeumorphicUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphicUIApp.swift
+//  NeumorphicUI
+//
+//  Created by user208584 on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphicUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
